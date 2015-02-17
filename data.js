885a0d5e72c0b30e818246fbf1f86d8562ca4ad9
zhukovka <helen.zhukova@gmail.com>
2015-02-17 18:07:00 +0200

    data

---
+++ data.js
@@ -0,0 +1,26 @@
+var data = {
+	bread: {
+		white: 10,
+		dark: 15,
+		ciabatta: 30
+	},
+	meat: {
+		ham: 40,
+		chicken: 30,
+		turkey: 35,
+		salmon: 50
+	},
+	fillings:{
+		onion: 5,
+		gherkins: 5,
+		tomato: 8,
+		cheese: 7,
+		lettuce: 6
+	},
+	sause{
+		cetchup: 5,
+		mayonnaise: 5,
+		mustard: 5,
+		thousandIslands: 15
+	}
+}
