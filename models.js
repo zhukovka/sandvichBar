0e97b74950edaca959a97f906569d3dbc7ab26a3
Roman <forthegranny@gmail.com>
2015-02-18 10:43:03 +0200

    added comments to model.js

--- models.js
+++ models.js
@@ -4,11 +4,17 @@ function Sandwich (obj) {
 	}
 }
 
+
 Sandwich.prototype.getPrice = function () {
 	var price = 0;
 
+    // Object.keys() method returns an array of a given object's own enumerable properties
+    // forEach() method executes a provided function (callback) once per array element.
+    // bind() method creates a new function that, when called, has its this keyword set to the provided value
 	Object.keys(this).forEach(dataKeys.bind(this));
 
+
+    //(el) is the currentValue (current element) being processed in the array.
 	function dataKeys(el){
 			if(Array.isArray(this[el])){
 				this[el].forEach(elementPrice(el));
@@ -22,8 +28,12 @@ Sandwich.prototype.getPrice = function () {
 			price += data[el][elt];
 		}
 	}
+
 	return price;
 }
 
-var s1 = new Sandwich({bread: 'white',
-meat:'ham', fillings: ['onion', 'tomato']});
+
+
+var s1 = new Sandwich({bread: 'white', meat:'ham', fillings: ['onion', 'tomato']});
+
+console.log(s1.getPrice());
