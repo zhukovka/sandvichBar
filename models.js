4e981923b8f46662ae9380f89240b99c881021fc
zhukovka <helen.zhukova@gmail.com>
2015-02-24 19:13:04 +0200

    parse figovaya data

--- models.js
+++ models.js
@@ -7,11 +7,9 @@ function Sandwich (obj) {
 Sandwich.prototype.getPrice = function () {
 	var price = 0;
 
-
 	Object.keys(this).forEach(elementPrice(), this);
 
 	function elementPrice(elt) {
-		
 		return function dataKeys(el){
 			if(Array.isArray(this[el])){
 				this[el].forEach(elementPrice(el));
@@ -28,3 +26,25 @@ Sandwich.prototype.getPrice = function () {
 
 var s1 = new Sandwich({bread: 'white',
 meat:'ham', fillings: ['onion', 'tomato']});
+
+function ChickenSandwich () {
+	var recipe = {bread: 'white',
+meat:'chicken', fillings: ['onion', 'tomato']};
+	Sandwich.call(this, recipe);
+	Object.defineProperties(this, {
+			  'bread': {
+			    value: this.bread,
+			    writable: false,
+			    enumerable: true
+			  },
+			  'meat': {
+			    value: this.meat,
+			    writable: false,
+			    enumerable: true
+			  }
+			});
+}
+
+ChickenSandwich.prototype = Object.create(Sandwich.prototype);
+
+var cs = new ChickenSandwich();
